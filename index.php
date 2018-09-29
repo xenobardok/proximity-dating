fe6dfb581fbe9cac20995e49482864264040dfda
Andrew Jelson <arjelson@go.olemiss.edu>
2018-09-29 08:07:58 -0500

    Added Header

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
      <!--Import materialize.css-->
      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+     <h1> Proximity</h1>
 
      <!--Let browser know website is optimized for mobile-->
      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
