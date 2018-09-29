16810413a93fa9916850161a820bc780337f2cdf
Andrew Jelson <arjelson@go.olemiss.edu>
2018-09-29 14:09:51 -0500

    cosmetic fix

--- successful.php
+++ successful.php
@@ -14,10 +14,10 @@
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">ProxiChats</a>
       <ul class="right">
-        <input placeholder="Status" id="status" type="text" class="validate">
+        <li><a href="index.php">Logout</a></li>
       </ul>
       <ul class="right">
-        <li><a href="index.php">Logout</a></li>
+        <input placeholder="Status" id="status" type="text" class="validate">
       </ul>
     </div>
   </nav>
