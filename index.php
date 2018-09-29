a14f3f07fa1f75cc5a18d5afb1248259d4aae685
Andrew Jelson <arjelson@go.olemiss.edu>
2018-09-29 10:43:06 -0500

    trial

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
       <ul class="right hide-on-med-and-down">
         <li><a href="#">Login</a></li>
       </ul>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Create Account</a></li>
+      </ul>
 
       <ul id="nav-mobile" class="sidenav">
         <li><a href="#">Create Account</a></li>
