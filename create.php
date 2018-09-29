99d63f47011ad8d0a2a6f11974d2eddcca56f27d
Andrew Jelson <arjelson@go.olemiss.edu>
2018-09-29 09:45:34 -0500

    Cosmetics

--- create.php
+++ create.php
@@ -27,38 +27,31 @@
     <form class="col s12">
       <div class="row">
         <div class="input-field col s6">
-          <input placeholder="First Name" id="first_name" type="text" class="validate">
-          <label for="first_name"></label>
+          <input placeholder="Name" id="name" type="text" class="validate">
+          <label for="name"></label>
         </div>
         <div class="input-field col s6">
-          <input placeholder="Last Name" id="last_name" type="text" class="validate">
-          <label for="last_name"></label>
+          <input placeholder="Username" id="username" type="text" class="validate">
+          <label for="username"></label>
         </div>
       </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Email" id="email" type="text" class="validate">
+          <label for="email"></label>
+        </div>
+        <div class="input-field col s6">
+          <input placeholder="Phone Number" id="phone_number" type="text" class="validate">
+          <label for="phone_number"></label>
+        </div>
       <div class="row">
         <div class="input-field col s6">
           <input placeholder="Password" id="password" type="password" class="validate">
           <label for="password"></label>
         </div>
-          <div class="input-field col s6">
+        <div class="input-field col s6">
             <input placeholder="Confirm Password" id="confirm_password" type="password" class="validate">
             <label for="confirm_password"></label>
-          </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col s12">
-          This is an inline input field:
-          <div class="input-field inline">
-            <input id="email_inline" type="email" class="validate">
-            <label for="email_inline">Email</label>
-            <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-          </div>
         </div>
       </div>
     </form>
