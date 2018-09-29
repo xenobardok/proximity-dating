5e14818a5a21efff32a95439fb649a17016bb9a6
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2018-09-29 17:49:26 -0400

    padding bottom removed

--- index.php
+++ index.php
@@ -54,16 +54,7 @@
 
 
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/main.js"></script>
-    <div id="fb-root"></div>
 
 
   </body>
