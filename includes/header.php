35e8b436a837aa9b5348310afd56468999177196
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2018-09-29 20:39:56 -0400

    Designed proximity

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="copyright" content="ProxiChats">
+    <link rel="shortcut icon" href="favicon.png">
+    <meta name="keywords" content="ProxiChats provides easy chatting solutions for users nearby">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="ProxiChats provides easy chatting solutions for users nearby">
+    <meta name="author" content="VOLHACKS 2018">
+
+    <title>ProxiChats</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="./css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="./css/main.css" rel="stylesheet">
