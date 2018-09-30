39a78ce50befd451884a130506112dd350cc21dd
Sunil Jamkatel <sunil.jamkatel@gmail.com>
2018-09-30 02:06:08 -0400

    Bug fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -3,7 +3,6 @@
 <nav class="purpl" role="navigation">
       <div class="nav-wrapper container"><a id="logo-container" href="index.php" class="brand-logo">ProxiChats</a>
       <?php if (basename($_SERVER['PHP_SELF']) == "messages.php" || basename($_SERVER['PHP_SELF']) == "proximity.php") {  
-          <?php 
           session_start();
       echo '<a class="user" style="float:right">Hi, '. $_SESSION['username'] . '</a>';
 
